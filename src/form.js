b2df08901f66a3b05628e6af38108dc819500a3f
Kevin Welcher <kaw2k@virginia.edu>
2015-11-23 17:41:02 -0500

    Adding in form serialize

--- src/form.js
+++ src/form.js
@@ -2,7 +2,12 @@ import React, { PropTypes } from 'react';
 import map from 'lodash.map';
 import values from 'lodash.values';
 import isEqual from 'lodash.isequal';
-
+import keys from './helpers/keys';
+import mapObj from './helpers/mapObj';
+import uniq from './helpers/uniq';
+import isNil from './helpers/isNil';
+import values from './helpers/values';
+import flatten from './helpers/flatten';
 
 export default React.createClass({
     propTypes: {
@@ -19,54 +24,30 @@ export default React.createClass({
     },
 
     serialize() {
-        console.log('===============');
-        const _refs = this.refs;
+        // We only care about things that can be serilized
+        let refs = [];
+        let refNameHash = {};
+        let refNameHashErrors = {};
+        for(let key in this.refs) {
+            if (!this.refs[key].serialize) continue;
 
-        // Clean out the supplied refs. We only care about things that can
-        // be serilized and are not themselves forms.
-        let refs = {};
-        let nestedFormsRefs = {};
-        for(let key in _refs) {
-            if (!_refs[key].serialize) continue;
-
-            if (_refs[key].isForm) {
-                nestedFormsRefs[key] = _refs[key];
-            } else {
-                refs[key] = _refs[key];
-            }
+            refs[key] = this.refs[key];
+            refNameHash[key] = undefined;
+            refNameHashErrors = [];
         }
 
-        // We want an object with all our refnames with undefined as values.
-        // This way our validators have placeholders for data
-        var refNameHash = map(() => undefined, refs);
-        var refNameHashErrors = map(() => [], refNameHash);
-
         // Set the initial form value
-        var form = {
+        let form = {
             valid: true,
             fieldValues: refNameHash,
             fieldErrors: refNameHashErrors,
             errors: []
         };
-        var oldForm = form;
 
-        var stable = false;
-        var iteration = 0;
-        var refLength = Object.keys(refs).length + Object.keys(nestedFormsRefs).length;
-
-        var mergeForm = function(formData, formRef) {
-            var serializedForm = formRef.serialize();
-
-            var fieldValues = Object.assign({}, formData.fieldValues, serializedForm.fieldValues);
-            var fieldErrors = Object.assign({}, formData.fieldErrors, serializedForm.fieldErrors);
-            var errors = [].concat.apply([], Object.values(fieldErrors))
-                            .filter(x => x)
-                            .reduce((items, item) => ~items.indexOf(item) ? items.concat(item) :items, []);
-
-            var valid = !errors.length;
-
-            return { fieldValues, fieldErrors, errors, valid };
-        };
+        let oldForm = form;
+        let stable = false;
+        let iteration = 0;
+        const refLength = keys(refs).length;
 
         while (iteration < refLength && !stable) {
             // Keep a copy of the previous iteration of the form so we can
@@ -74,12 +55,17 @@ export default React.createClass({
             oldForm = Object.assign({}, form);
 
             // Get all the values of the form in no particular order
-            form.fieldValues = map(ref => ref.serialize(form.fieldValues), refs);
+            form.fieldValues = mapObj(ref => ref.serialize(form.fieldValues), refs);
 
             // Get all the errors for the fields
-            form.fieldErrors = map((ref, name) => {
-                var hasIsDisabled = ref.hasOwnProperty('isDisabled');
-                var hasIsDisabledResult = hasIsDisabled && ref.isDisabled(form.fieldValues[name], form.fieldValues, form.fieldErrors);
+            form.fieldErrors = mapObj((ref, name) => {
+                const hasIsDisabled = ref.hasOwnProperty('isDisabled');
+
+                let hasIsDisabledResult = hasIsDisabled && ref.isDisabled(
+                    form.fieldValues[name],
+                    form.fieldValues,
+                    form.fieldErrors
+                );
 
                 if (hasIsDisabled && hasIsDisabledResult) { return []; }
                 if (!hasIsDisabled && ref.props.disabled) { return []; }
@@ -88,26 +74,26 @@ export default React.createClass({
                 // will let the component add its own validators and set them
                 // to state and allow the parent to supply them w/o the child
                 // caring via props
-                var stateValidators = (ref.state && ref.state.validators) || [];
-                var propValidators = ref.props.validators || [];
-                var validators = [].concat(stateValidators, propValidators);
+                const stateValidators = (ref.state && ref.state.validators) || [];
+                const propValidators = ref.props.validators || [];
+                const validators = [].concat(stateValidators, propValidators);
 
                 // Get all the error messages and remove nulls
                 return validators
                         .map(validator => validator(form.fieldValues[name], form.fieldValues, form.fieldErrors))
-                        .filter(val => val);
+                        .filter(x => x);
             }, refs);
 
             // Provide a flattened list of uniq errors for easy UI display
-            form.errors = [];
+            form.errors = uniq(flatten(values(form.fieldErrors)))
+                            .filter(x => !isNil(x));
 
             // Have a helper property to detect if the form is overall valid
             form.valid = !form.errors.length;
 
-            form = values(nestedFormRefs).reduce(mergeForm, form);
-
             // Set ourselves up for the next iteration
-            stable = isEqual(form, oldForm);
+            // TODO: Evenatually we will need to check equality
+            stable = false;
             iteration++;
         }
 
