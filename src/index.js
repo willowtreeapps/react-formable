bbe3f0c8ec38cf24d36f835dc254bc66b515b48c
Kevin Welcher <kaw2k@virginia.edu>
2015-11-24 11:54:55 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 
 import Form from './form';
 import Input from './inputs/input';
+import Test from './inputs/test';
 
 export class App extends React.Component {
 	render() {
         return <Form>
 			<Input type="text" name="test" format={x => x.toUpperCase()}/>
+			<Test type="text" name="test" format={x => x.toUpperCase()}/>
         </Form>;
 	}
 }
