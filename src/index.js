c01e877b8b670a8c14d97f2a1fad89bed12c9598
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2015-11-24 16:05:50 -0500

    test package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // Form
-import From from './form';
+import FForm from './form';
 
 // Inputs
 import input from './inputs/input';
 import test from './inputs/test';
 
-export From;
+export const Form = FForm;
 
 export const Inputs = {
 	input: input,
