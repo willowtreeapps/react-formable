400e210b3920972c5deeceb5c8c4059635b731db
Matt Uttridge <matt.uttridge@willowtreeapps.com>
2015-12-24 15:17:38 +0000

    description

--- example/src/controllers/examples/subsections/FormData.js
+++ example/src/controllers/examples/subsections/FormData.js
@@ -10,6 +10,7 @@ export default class FormData extends React.Component {
         const formData = this.props.data ? JSON.stringify(this.props.data, null, 2) : '{}';
 
         return <div>
+            <p>As you fill out the form you can see the model update below</p>
             <pre><code class="json">{formData}</code></pre>
         </div>;
     }
