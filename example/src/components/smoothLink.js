46b9b0ab8b2f38491e0e68b8f8ce03c2e89ac070
Kevin Welcher <kaw2k@virginia.edu>
2015-12-24 00:07:04 -0500

    Tidying up sidebar a bit
    Now sidebar behaves nicely when footer is in view. Also adding smooth
    scroll for subsection links

---
+++ example/src/components/smoothLink.js
@@ -0,0 +1,17 @@
+/*eslint func-style:0*/
+import React, { PropTypes } from 'react';
+import smoothScroll from 'smoothscroll';
+
+export default function SmoothLink({ link, title }) {
+    function onClick(event) {
+        event.preventDefault();
+        smoothScroll(document.querySelector(`#${link}`));
+    }
+
+    return <a href={`#${link}`} onClick={onClick}>{title}</a>;
+}
+
+SmoothLink.propTypes = {
+    link: PropTypes.string,
+    title: PropTypes.string
+};
