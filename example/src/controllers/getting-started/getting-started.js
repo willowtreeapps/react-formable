1bc882b9ff8574206c8050828f6d20653110a2ea
Kevin Welcher <kaw2k@virginia.edu>
2015-12-28 11:18:02 -0500

    Adding getting started content

--- example/src/controllers/getting-started/getting-started.js
+++ example/src/controllers/getting-started/getting-started.js
@@ -1,12 +1,59 @@
 import React, { PropTypes } from 'react';
 import Page from '../../components/page';
-// import * as Basic from './subsections/basic';
+//import * as Basic from './subsections/basic';
 // import * as Advanced from './subsections/advanced';
 
 const subsections = [
-    // Basic,
+    //Basic,
     // Advanced
 ];
+
+const description = `
+Looking to get started with react-formable? Awesome!
+
+## Installation
+
+**NPM**
+
+    npm install react-formable --save
+
+**Bower**
+
+    bower install react-formable --save
+
+## Quickstart
+
+We can include the library in several ways.
+
+    // ES6 Imports
+    import Form, { Input, Errors } from 'react-formable';
+
+    // require
+    var Formable = require('react-formable');
+    var Form = Formable.Form;
+    var Input = Formable.Input;
+    var Errors = Formable.Errors;
+
+    // require with de-structuring
+    var { Form, Input, Errors } = require('react-formable');
+
+Now lets render a simple login form that will display errors.
+
+    const LoginForm = React.createClass({
+        onSubmit(form) {
+            console.log(form);
+        },
+
+        render() {
+            return <Form onSubmit={this.onSubmit}>
+                <Input name="username" type="text" />
+                <Input name="password" type="password" />
+                <button>Login</button>
+                <Errors />
+            </Form>;
+        }
+    });
+`;
 
 export default React.createClass({
     propTypes: {
@@ -21,6 +68,7 @@ export default React.createClass({
 
     render() {
         return <Page title="Getting Started"
+                     description={description}
                      className="getting-started"
                      subsections={subsections} />
     }
