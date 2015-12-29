22615a928a35f19e61e15be587fdb2e9fded04ca
Kevin Welcher <kaw2k@virginia.edu>
2015-12-29 11:49:10 -0500

    Adding placeholders for new guides

--- example/src/controllers/guides/guides.js
+++ example/src/controllers/guides/guides.js
@@ -1,7 +1,19 @@
 import React, { PropTypes } from 'react';
 import Page from '../../components/page';
+import * as FormWalkThrough from './subsections/form-walk-through';
+import * as AddingValidation from './subsections/adding-validation';
+import * as DisplayErrors from './subsections/displaying-errors';
+import * as CreatignInputs from './subsections/creating-inputs';
+import * as ReusableFormSections from './subsections/reusable-form-sections';
+import * as HighorderForms from './subsections/highorder-forms';
 
 const subsections = [
+    FormWalkThrough,
+    AddingValidation,
+    DisplayErrors,
+    CreatignInputs,
+    ReusableFormSections,
+    HighorderForms
 ];
 
 export default React.createClass({
---
+++ example/src/controllers/guides/subsections/adding-validation.js
@@ -0,0 +1,5 @@
+export const title = 'Adding Validation';
+export const link = 'adding-validation';
+
+export const markdown = `
+`;
