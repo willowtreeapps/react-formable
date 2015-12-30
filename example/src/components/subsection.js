8a29e64fc53abb74c4507b88a6462c3ac7ace568
Matt Uttridge <matt.uttridge@willowtreeapps.com>
2015-12-30 10:38:20 -0500

    Subsection refactor

--- example/src/components/subsection.js
+++ example/src/components/subsection.js
@@ -3,21 +3,14 @@ import React, { PropTypes } from 'react';
 import MD  from './md';
 
 
-function renderSubsections(subSections =[]) {
-    return subSections.map(subsection => {
-        return new Subsection(subsection);
-    });
-}
-
 export default function Subsection(props) {
     const {
         title='',
         markdown,
         link,
-        subSections,
+        subSections=[],
         code: Code
     } = props;
-
 
     return <div id={link} className="subsection">
         {title && title.length && <h3>{title}</h3>}
@@ -25,7 +18,9 @@ export default function Subsection(props) {
         {markdown && <MD text={markdown} />}
 
         {Code && <Code />}
-        {renderSubsections(subSections)}
+        {subSections.map((subSection) => {
+            return <Subsection key={subSection.link} {...subSection} />;
+        })}
     </div>;
 }
 
