006ee285edc8f7e8b20a1768e7cafee2d5a93bb3
Matt Uttridge <matt.uttridge@willowtreeapps.com>
2015-12-30 15:38:31 -0500

    failing test for nested with dot notation

--- src/validators/__tests__/equalsField-test.js
+++ src/validators/__tests__/equalsField-test.js
@@ -1,17 +1,43 @@
 jest.dontMock('../equalsField');
 
-const equalsField = require('../equalsField').default('password2', 'must be equal');
+const equalsField = require('../equalsField').default;
 
 describe('required', () => {
+
+    let validator = equalsField('password2', 'must be equal');
+
     it('returns nothing when other form field is equal', () => {
-        expect(equalsField('ABC', {
+        expect(validator('ABC', {
             password2: 'ABC'
         })).toBe(undefined);
     });
 
     it('returns error message when form field is different', () => {
-        expect(equalsField('ABC', {
+        expect(validator('ABC', {
             password2: 'DEF'
         })).toBe('must be equal');
     });
+
+    validator = equalsField('deep.nested.field', 'must be equal');
+
+    it('returns nothing when other nested form field is equal', () => {
+        expect(validator('ABC', {
+            deep: {
+                nested: {
+                    field: 'ABC'
+                }
+            }
+        })).toBe(undefined);
+    });
+
+    it('returns error message when nested form field is different', () => {
+        expect(validator('ABC', {
+            deep: {
+                nested: {
+                    field: 'DEF'
+                }
+            }
+        })).toBe('must be equal');
+    });
+
 });
