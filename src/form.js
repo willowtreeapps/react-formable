05099e809a138d1eb096bb8398e7a012da91fa6c
Kevin Welcher <kaw2k@virginia.edu>
2015-12-31 10:40:15 -0500

    Changing params for form validation

--- src/form.js
+++ src/form.js
@@ -227,7 +227,7 @@ export default React.createClass({
             (this.props.validators || []),
             (this.validators || [])
         );
-        const formErrors = formValidators.map(fn => fn(form));
+        const formErrors = formValidators.map(fn => fn(form.fieldValues, form.fieldValues, form.fieldErrors));
 
         form.errors = uniq(form.errors.concat(formErrors));
         form.valid = !form.errors.length;
