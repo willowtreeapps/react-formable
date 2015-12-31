b12fe84405f6951fd9d2b3f8d8b5328b7bf0c98e
Matt Uttridge <matt.uttridge@willowtreeapps.com>
2015-12-31 15:02:17 -0500

    api level 1

--- example/src/controllers/api/subsections/errors.js
+++ example/src/controllers/api/subsections/errors.js
@@ -1,7 +1,4 @@
-export const title = 'Errors';
-export const link = 'errors';
-
-export const markdown = `
+const markdown = `
 A component which soaks up and displays form errors. You can think of \`Errors\` like it is a placeholder. Wherever you place it, errors will be rendered there.
 
 **NB**: Make sure to place this component *within* the \`Form\` tag.
@@ -12,3 +9,7 @@ A component which soaks up and displays form errors. You can think of \`Errors\`
 | additionalErrors | array[string] | [] | Any additional errors you would want to render to the screen can be passed down as an array of strings. |
 | renderError | function(error) => node | identity | If you want to overwrite how errors are rendered, you can do so by providing a callback to errors. This function will receive each error and will return what you want to be rendered as your error. |
 `;
+
+export const content = [markdown];
+export const title = 'Errors';
+export const link = 'errors';
