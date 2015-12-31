89bc7d9d71be1404151f442d9dad3cf0dce0cd42
Matt Uttridge <matt.uttridge@willowtreeapps.com>
2015-12-31 11:56:35 -0500

    freeing up presentation

--- example/src/controllers/examples/subsections/signup/index.js
+++ example/src/controllers/examples/subsections/signup/index.js
@@ -1,4 +1,12 @@
+import React from 'react';
+import SimpleSection from '../../../../components/SimpleSection';
+
 export const title = 'Signup';
 export const link = 'Signup';
 export const markdown = require('../../samples').markdown.signup
 export const code = require('../../samples').code.signup
+
+/*eslint func-style:0*/
+export function content() {
+    return <SimpleSection markdown={markdown} code={code} />
+}
