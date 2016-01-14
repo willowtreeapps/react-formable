15af4941b4fc02f69d5f4229cda15088d4f610c2
Matt Uttridge <matt.uttridge@willowtreeapps.com>
2016-01-14 15:29:46 -0500

    test for form keydown

---
+++ src/__tests__/form-test.js
@@ -0,0 +1,33 @@
+jest.dontMock('../form');
+jest.dontMock('../inputs/input');
+
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+const Form = require('../form').default;
+const Input = require('../inputs/input').default;
+
+describe('Form', () => {
+    it('submits if the user hits enter', () => {
+        const onSubmit = jest.genMockFn();
+        let form = TestUtils.renderIntoDocument(
+            <Form onSubmit={onSubmit}>
+                <label> Pet Name: <Input className="petname" name="petname"
+                type="text" /> </label>
+            </Form>
+        );
+
+        const inputNode = TestUtils.findRenderedDOMComponentWithClass(form, 'petname');
+
+        inputNode.value = 'giraffe'
+        TestUtils.Simulate.change(inputNode);
+        TestUtils.Simulate.keyDown(inputNode, { key: 'Enter',
+            keyCode: 13, which: 13 });
+
+        expect(onSubmit).toBeCalled();
+        expect(onSubmit.mock.calls[0][0].fieldValues).toEqual({
+            petname: 'giraffe'
+        });
+    });
+});
