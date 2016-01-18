d15d13d96619302ba3d964990a3f4d47ddf09964
Matt Uttridge <matt.uttridge@willowtreeapps.com>
2016-01-18 13:36:43 -0500

    parking

--- src/__tests__/fieldlist-test.js
+++ src/__tests__/fieldlist-test.js
@@ -5,11 +5,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
-const Fieldlist = require('../fieldset').default;
+const Fieldlist = require('../fieldlist').default;
 const Input = require('../inputs/input').default;
 
 describe('Fieldlist', () => {
     it('write test', () => {
-        expect(1).toBe(2);
+        let fieldlist = TestUtils.renderIntoDocument(
+            <Fieldlist name="pets">
+                <div>
+                    <label> Pet Name: <Input name="name" type="text" /> </label>
+                    <label> Pet Type: <Input name="type" type="text" /> </label>
+                </div>
+            </Fieldlist>
+        );
+        const fieldlistNode = ReactDOM.findDOMNode(fieldlist);
+
+        expect(fieldlistNode).not.toBeNull();
     });
 });
