74d42a2f7a5c95e6ad2ad17b52be34c00aa169cb
Kevin Welcher <kaw2k@virginia.edu>
2016-01-25 14:43:57 -0500

    Updating docs to reflect new changes

--- example/src/controllers/api/subsections/form.js
+++ example/src/controllers/api/subsections/form.js
@@ -7,7 +7,8 @@ The top level \`Form\` component is what serializes your data.
 | onSubmit | function(form) | undefined | A callback which will be called whenever the form is submitted. Receives the serialized form object. |
 | showErrorsOnSubmit | boolean | true | A boolean to decide if errors should be shown on submit. |
 | showErrorsOnChange | boolean(form) | false | A boolean to decide if errors should be shown on change. |
-| validators | array[function(form)] | [] | An array of validators to run over the form. Useful to capture business logic. Not automatically bound to the form. |
+| validators | array[function] | [] | An array of validators to run over the form. Useful to capture business logic. |
+| addValidationErrors | boolean | false | Add \`fieldErrors\` to your validators. This allows you to reference other components field errors within your validation methods as its last parameter. |
 
 
 There are a handful of methods on the \`Form\` component which are useful. To access these, attach a \`ref\` to the \`Form\` and call them via \`this.refs.refName.methodName();\`.
