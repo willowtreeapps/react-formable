25a65a00b32d3c40803eaab8d1454217eaea6071
Kevin Welcher <kaw2k@virginia.edu>
2016-04-13 12:19:01 -0400

    :bug: Fixes error with passing down props in Fieldset
    We forgot to pass along submit and on change

--- src/fieldset.js
+++ src/fieldset.js
@@ -31,7 +31,12 @@ export default React.createClass({
     render() {
         warning( this.props.name, `Fieldset found without a name prop. The children of this component will behave eratically` );
         const errorsRule = createErrorsRule(this.props.errors, this.props.fieldErrors);
-        const formableRule = createFormableRule(this.props.errors, this.props.fieldErrors);
+        const formableRule = createFormableRule(
+            this.props.errors,
+            this.props.fieldErrors,
+            this.props.onSubmit,
+            this.props.onChange
+        );
 
         return <div {...this.props}>
             {cloneChildren([errorsRule, formableRule], this.props.children)}
