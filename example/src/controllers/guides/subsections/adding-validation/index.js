ba4fce2cf9f020618ec725cb70c39b66a89c7600
Kevin Welcher <kaw2k@virginia.edu>
2016-06-16 13:30:30 -0400

    :fire:

--- example/src/controllers/guides/subsections/adding-validation/index.js
+++
@@ -1,6 +0,0 @@
-/*eslint prefer-template:0*/
-export const title = 'Adding Validation';
-export const link = 'adding-validation';
-export const content = [
-    require('fs').readFileSync(__dirname + '/description.md', 'utf8')
-];
--- example/src/controllers/guides/subsections/creating-inputs/index.js
+++
@@ -1,8 +0,0 @@
-/*eslint prefer-template:0*/
-
-export const title = 'Creating Inputs';
-export const link = 'creating-inputs';
-
-export const content = [
-    require('fs').readFileSync(__dirname + '/description.md', 'utf8')
-];
