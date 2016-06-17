f8a1620d88bbdb715e29287193375c4a01883163
Kevin Welcher <kaw2k@virginia.edu>
2016-06-17 16:20:15 -0400

    :tada: Adds jest for testing

--- src/example.tsx
+++ src/example.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import Formable from './root';
 
 export default class ComponentExample extends React.Component<{}, {}>  {
     public render(): React.ReactElement<{}> {
-        return <Formable projectName="react-formable" />;
+        return <div>SAD</div>;
     }
 }
