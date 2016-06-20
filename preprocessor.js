3d10a02a3b6da334a0a7fcf6a47d9c142b3c18a0
Kevin Welcher <kaw2k@virginia.edu>
2016-06-20 16:14:36 -0400

    :art: More work on unit tests

--- preprocessor.js
+++ preprocessor.js
@@ -1,7 +1,17 @@
 const tsc = require('typescript');
+const babel = require('babel-core');
 
 module.exports = {
-    process(src, path) {
+    process: function(src, path) {
+        if (path.endsWith('.js')) {
+            return babel.transform(src, {
+                filename: path,
+                presets: ['es2015', 'react'],
+                retainLines: true,
+                compact: false
+            }).code;
+        }
+
         if (path.endsWith('.ts') || path.endsWith('.tsx')) {
             return tsc.transpile(
                 src,
