0ac216a55869f3302bad1ea06ab05ccd86554fdd
Kevin Welcher <kaw2k@virginia.edu>
2016-06-24 22:23:18 -0400

    :tada: Adds an example

--- src/example.tsx
+++ src/example.tsx
@@ -1,7 +1,27 @@
 import * as React from 'react';
+import { Form, Input, Errors} from './root';
 
-export default class ComponentExample extends React.Component<{}, {}>  {
+interface IState {
+    name: string;
+}
+
+export default class ComponentExample extends React.Component<{}, IState>  {
+    public state: IState = {
+        name: ''
+    };
+
     public render(): React.ReactElement<{}> {
-        return <div>SAD</div>;
+        return <Form onChange={form => this.setState({ name: form.fieldValues.name })}
+                     onSubmit={form => console.log(form)}>
+            <Input name="name"
+                   value={this.state.name}
+                   validators={[
+                       value => !value.length && 'bad'
+                   ]}/>
+
+            <span>{this.state.name}</span>
+
+            <Errors />
+        </Form>;
     }
 }
