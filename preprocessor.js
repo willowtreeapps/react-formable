8ca1f7dc0d05120522a748bbd5fdf7bd4fcf3e78
Kevin Welcher <kaw2k@virginia.edu>
2016-06-24 22:17:33 -0400

    :art: Switching from sapling to jest for testing

---
+++ preprocessor.js
@@ -0,0 +1,29 @@
+const tsc = require('typescript');
+const babel = require('babel-core');
+
+module.exports = {
+    process(src, path) {
+        if (path.endsWith('.js')) {
+            return babel.transform(src, {
+                filename: path,
+                presets: ['es2015', 'react'],
+                retainLines: true,
+                compact: false
+            }).code;
+        }
+
+        if (path.endsWith('.ts') || path.endsWith('.tsx')) {
+            return tsc.transpile(
+                src,
+                {
+                    module: tsc.ModuleKind.CommonJS,
+                    jsx: tsc.JsxEmit.React
+                },
+                path,
+                []
+            );
+        }
+
+        return src;
+    }
+};
