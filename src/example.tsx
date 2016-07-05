031ca40da780139f5e4cd5aac178b358be0eba01
Kevin Welcher <kaw2k@virginia.edu>
2016-07-04 23:23:33 -0400

    :art: Bumps the example some

--- src/example.tsx
+++ src/example.tsx
@@ -3,26 +3,35 @@ import { Form, Input, Errors} from './root';
 
 interface IState {
     name: string;
+    form: any;
 }
 
 export default class ComponentExample extends React.Component<{}, IState>  {
     public state: IState = {
-        name: ''
+        name: '',
+        form: null
     };
 
     public render(): React.ReactElement<{}> {
-        return <Form onChange={form => this.setState({ name: form.fieldValues.name })}
+        return <Form onChange={form => this.setState({ name: form.fieldValues.name, form })}
                      onSubmit={form => console.log(form)}
                      showErrorsOnChange>
-            <Input name="name"
-                   value={this.state.name}
-                   validators={[
-                       value => !value.length && 'bad'
-                   ]}/>
+            <div className="half">
+                <Input name="name"
+                       value={this.state.name}
+                       autoComplete="off"
+                       validators={[
+                           value => !value.length && 'bad'
+                       ]}/>
 
-            <span>{this.state.name}</span>
+                <span>{this.state.name}</span>
 
-            <Errors />
+                <Errors />
+            </div>
+
+            <pre className="half">
+                {JSON.stringify(this.state.form, null, '  ')}
+            </pre>
         </Form>;
     }
 }
