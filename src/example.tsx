841069b140c4ebf284b003bc1b5fcaf83a355579
Kevin Welcher <kaw2k@virginia.edu>
2016-07-04 23:25:56 -0400

    :art: One more example bump

--- src/example.tsx
+++ src/example.tsx
@@ -2,29 +2,23 @@ import * as React from 'react';
 import { Form, Input, Errors} from './root';
 
 interface IState {
-    name: string;
     form: any;
 }
 
 export default class ComponentExample extends React.Component<{}, IState>  {
     public state: IState = {
-        name: '',
         form: null
     };
 
     public render(): React.ReactElement<{}> {
-        return <Form onChange={form => this.setState({ name: form.fieldValues.name, form })}
-                     onSubmit={form => console.log(form)}
-                     showErrorsOnChange>
+        return <Form showErrorsOnChange
+                     onChange={form => this.setState({ form })}>
             <div className="half">
                 <Input name="name"
-                       value={this.state.name}
                        autoComplete="off"
                        validators={[
                            value => !value.length && 'bad'
                        ]}/>
-
-                <span>{this.state.name}</span>
 
                 <Errors />
             </div>
