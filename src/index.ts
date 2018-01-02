fde826e1f0ccae8a1d243ce6ce9e659d492f2c4b
Kevin Welcher <kaw2k@virginia.edu>
2018-01-02 11:50:00 -0500

    :art: Exports additional types from the base file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import Form from './Form'
 export default Form
 
-export { Form, Props as FormProps } from './Form'
+export {
+  Form,
+  Props as FormProps,
+  FieldErrors,
+  Validation,
+  FieldValues,
+} from './Form'
 export { Errors, Props as ErrorProps } from './Errors'
 export { Input, Props as InputProps } from './Input'
